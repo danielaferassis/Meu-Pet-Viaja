084a1dc0ff2cc4d05b825308def09512ca4b94b1
Ana Carolina Fernandes de Assis <assiscarolina18@gmail.com>
2023-06-11 20:02:41 -0300

    atualização cadastro estabelecimento
    Permitindo carregar múltiplas fotos #72

--- src/js/cadastro_estabelecimento.js
+++ src/js/cadastro_estabelecimento.js
@@ -37,3 +37,28 @@ function mostrarSenha(){
         senha.type='password';
 }
 };
+
+const fileInput = document.querySelector('#foto_estabelecimemto');
+
+const uploadFile = (files) => {
+  const API_ENDPOINT = "https://file.io";
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+
+  request.open("POST", API_ENDPOINT, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);
+    }
+  };
+  
+  for (let i = 0; i < files.length; i++) {
+    formData.append(files[i].name, files[i])
+  }
+  request.send(formData);
+};
+
+fileInput.addEventListener("change", event => {
+  const files = event.target.files;
+  uploadFile(files);
+});

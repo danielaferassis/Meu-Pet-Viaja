1d9087697f5ec75d49ddbee1dd138a58994a1111
lucas-rodrigo <lucas.goncalves.1438705@sga.pucminas.br>
2023-07-02 18:02:19 -0300

    #75

--- src/JS/alterar_senha.js
+++ src/JS/alterar_senha.js
@@ -1,46 +1,59 @@
-
-
-
 function enviar() {
-    const senha = document.getElementById("senha").value;
-    const confirmarSenha = document.getElementById("confirmarSenha").value;
-  
-    if (senha !== confirmarSenha) {
-      alert("As senhas não coincidem. Por favor, verifique.");
-      return;
-    }
-  
-    alterarSenha(senha);
-  }
-  
+  // Obter os valores dos campos de entrada
+  var email = document.getElementById("email").value;
+  var novaSenha = document.getElementById("senha").value;
+  var confirmarSenha = document.getElementById("confirmarSenha").value;
 
-  function alterarSenha(senha) {
-    const usuarioId = 1; // ID do usuário que deseja alterar a senha
-    const url = `http://localhost:3000/viajante/${usuarioId}`;
-  
-    // Definir o corpo da requisição com a nova senha
-    const body = {
-      senha: senha
-    };
-  
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-      .then(response => {
-        if (response.ok) {
-          alert("Senha alterada com sucesso!");
-        } else {
-          alert("Erro ao alterar a senha. Por favor, tente novamente.");
-        }
-      })
-      .catch(error => {
-        console.error('Ocorreu um erro:', error);
-        alert("Erro ao alterar a senha. Por favor, tente novamente.");
-      });
+  // Validar os valores de entrada
+  if (email === "") {
+    alert("Preencha o e-mail!");
+    return;
   }
-  
-  
+
+  if (novaSenha === "") {
+    alert("Preencha a nova senha!");
+    return;
+  }
+
+  if (novaSenha !== confirmarSenha) {
+    alert("As senhas não coincidem!");
+    return;
+  }
+
+  // Buscar os dados JSON na API
+  fetch("http://localhost:3000/viajante")
+    .then(response => response.json())
+    .then(data => {
+      // Encontrar o usuário com o e-mail fornecido
+      var usuario = data.find(viajante => viajante.email === email);
+
+      if (usuario) {
+        // Atualizar a senha do usuário
+        usuario.senha = novaSenha;
+
+        // Enviar os dados JSON atualizados de volta para a API
+        fetch("http://localhost:3000/viajante", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+        })
+          .then(response => response.json())
+          .then(dadosAtualizados => {
+            console.log("Senha atualizada com sucesso:", dadosAtualizados);
+            // Exibir mensagem de sucesso ou executar outras ações
+          })
+          .catch(error => {
+            console.error("Erro ao atualizar a senha:", error);
+            // Exibir mensagem de erro ou executar outras ações
+          });
+      } else {
+        alert("Usuário não encontrado!");
+      }
+    })
+    .catch(error => {
+      console.error("Erro ao buscar os dados JSON:", error);
+      // Exibir mensagem de erro ou executar outras ações
+    });
+}

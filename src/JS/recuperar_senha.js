1ed5b2f1824982acfcdc80a7bad510d3c0232e28
lucas-rodrigo <lucas.goncalves.1438705@sga.pucminas.br>
2023-07-02 14:00:27 -0300

    #75
    Criação de pagina html para redefinição de senha

--- src/JS/recuperar_senha.js
+++ src/JS/recuperar_senha.js
@@ -1,3 +1,16 @@
+/* 
+Verificar se o email está cadastrado no json
+  Se não: verifique o email digitado
+  Se sim: ser redirecionado para pagina de redefinição de senha
+  Inserir nova senha e conferir se são iguais
+  se não: As duas senhas devem ser iguais
+  Se sim: é enviada a alteração para o json
+  e o usuario recebe msg de senha alterada com sucesso
+*/
+
+
+
+
 function enviarLinkRedefinicaoSenha(email) {
     // Implementar a lógica para enviar o link de redefinição de senha para o email cadastrado
     // Atraves de uma API de envio de emails ou um serviço de envio de emails específico
